fff9a57038daad3534466032c37e34b9843f7ac9
peatchXD <100938925+peatchXD@users.noreply.github.com>
2023-04-20 04:02:46 +0700

    Update All-in-one.lua

--- All-in-one.lua
+++ All-in-one.lua
@@ -1,12 +1,11 @@
-local gamelist = {
-    [537413528]  = "https://raw.githubusercontent.com/GODMODs132/Build-A-Boat-For-Treasure/main/Script.lua",
-    [13055060173] = "https://raw.githubusercontent.com/GODMODs132/SCRATCH-SERIES/main/SCRATCH_SERIES.lua",
-    [11233292886] = "https://raw.githubusercontent.com/GODMODs132/XIN-TOWN/main/Script.lua",
-}
-
-local checkgame = gamelist[game.gameId]
-if gamelist[game.gameId] then 
-    loadstring(game:HttpGet(checkgame))()
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 537413528 or PlaceId == 1930665568 or PlaceId == 1930863474 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/Build-A-Boat-For-Treasure/main/Script.lua")()
+elseif PlaceId == 13055060173 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/SCRATCH-SERIES/main/SCRATCH_SERIES.lua")()
+elseif PlaceId == 11233292886 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/XIN-TOWN/main/Script.lua")()
 else 
-game.Players.LocalPlayer:Kick("กูไม่ได้ทำไอส้สรันหาแม่มึงอะ")
+    game.Players.LocalPlayer:Kick("กูไม่ได้ทำไอส้สรันหาแม่มึงอะ")
 end

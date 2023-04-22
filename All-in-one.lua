822e62b2002cff56b847fa06ea4beb69b7567628
peatchXD <100938925+peatchXD@users.noreply.github.com>
2023-04-22 16:54:21 +0700

    Update All-in-one.lua

--- All-in-one.lua
+++ All-in-one.lua
@@ -2,8 +2,10 @@ repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 537413528 or PlaceId == 1930665568 or PlaceId == 1930863474 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/Build-A-Boat-For-Treasure/main/Script.lua")()
-elseif PlaceId == 13055060173 or PlaceId == 13212005672 then
+elseif PlaceId == 13055060173 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/SCRATCH-SERIES/main/SCRATCH_SERIES.lua")()
+elseif PlaceId == 13212005672 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/SCRATCH-MULTIVERSE/main/Script.lua")()
 elseif PlaceId == 11233292886 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/XIN-TOWN/main/Script.lua")()
 elseif PlaceId == 9853584748 then

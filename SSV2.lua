c73449c1e670d0c6b087c5d1affe23a5617c4582
peatchXD <100938925+peatchXD@users.noreply.github.com>
2023-04-22 17:01:20 +0700

    Update SSV2.lua

--- SSV2.lua
+++ SSV2.lua
@@ -57,7 +57,7 @@ game:GetService("ReplicatedStorage").pNotifyStorage.pNotifyEvent:FireServer(unpa
 ------------------------------------------------------------------------------------------
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure/main/GUI"))() 
-local Window = Library.CreateLib("SCRATCH MULTIVERSE | NONAME HUB", "DarkTheme")
+local Window = Library.CreateLib("SCRATCH SERIES | NONAME HUB", "DarkTheme")
 
 ------------------------------------------------------------------------------------------
 
@@ -65,7 +65,7 @@ local Tab = Window:NewTab("Admin")
 
 local Section = Tab:NewSection("Admin Menu")
 
-game:GetService("Players").Tljop838.PlayerGui["Admin gui"].ResetOnSpawn = false
+game:GetService("Players").LocalPlayer.PlayerGui["Admin gui"].ResetOnSpawn = false
 
 Section:NewToggle("Open Admin UI", "เมนูเสกของแอดมิน", function(AdminGui)
 local Admin_Menu = game:GetService("Players").LocalPlayer

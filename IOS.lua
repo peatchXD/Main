5dc3931c4786e238c9e56ac1117494cd935f2e51
peatchXD <100938925+peatchXD@users.noreply.github.com>
2023-05-11 21:10:34 +0700

    Create IOS.lua

---
+++ IOS.lua
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/GODMODs132/NONAME-HUB/main/Script.lua"),true))()
+
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()

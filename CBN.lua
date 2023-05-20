7914b5a21cec92542f732d4571b4c1edb1c587bf
peatchXD <100938925+peatchXD@users.noreply.github.com>
2023-05-20 21:24:33 +0700

    Create CBN.lua

---
+++ CBN.lua
@@ -0,0 +1,534 @@
+local allowExecution = true
+
+if not allowExecution then
+    game.Players.LocalPlayer:Kick("Script Execution is Currently Locked. [Dm: peatchXD#6075]")
+    return
+end
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure/main/GUI"))() 
+local Window = Library.CreateLib("🌺City BanNa🌼 | NONAME HUB (Beta 1.0)", "DarkTheme")
+
+local Tab = Window:NewTab("Admin")
+
+local Section = Tab:NewSection("Admin")
+
+Section:NewButton("Bypass", " ",function()
+
+for i,v in pairs(workspace:GetDescendants()) do
+    if v.Name == "AntiSpeed" then
+        v:Destroy()
+    end
+end
+for i,v in pairs(workspace:GetDescendants()) do
+    if v.Name == "WalkSpeedInputScript" then
+        v:Destroy()
+    end
+end
+for i,v in pairs(workspace:GetDescendants()) do
+    if v.Name == "JumpDebouncer" then
+        v:Destroy()
+    end
+end
+end)
+
+local Section = Tab:NewSection("Vehicle Fly")
+
+Section:NewButton("Vehicle Fly (B)", "Press Key B to Stop Fly Press B to Start Fly",function()
+game:GetService('Players').LocalPlayer.Character.Humanoid.Name = "Humanoida"
+repeat wait()
+until game:GetService"Players".LocalPlayer and game:GetService"Players".LocalPlayer.Character and game:GetService"Players".LocalPlayer.Character:findFirstChild("UpperTorso") and game:GetService"Players".LocalPlayer.Character:findFirstChild("Humanoida")
+local mouse = game:GetService"Players".LocalPlayer:GetMouse()
+repeat wait() until mouse
+local plr = game:GetService"Players".LocalPlayer
+local torso = plr.Character.UpperTorso
+local flying = true
+local deb = true
+local ctrl = {f = 0, b = 0, l = 0, r = 0}
+local lastctrl = {f = 0, b = 0, l = 0, r = 0}
+local maxspeed = 100
+local speed = 100
+
+function Fly()
+local bg = Instance.new("BodyGyro", torso)
+bg.P = 9e4
+bg.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+bg.cframe = torso.CFrame
+local bv = Instance.new("BodyVelocity", torso)
+bv.velocity = Vector3.new(0,0.1,0)
+bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
+repeat wait()
+--plr.Character.Humanoida.PlatformStand = true
+if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then
+speed = speed+.5+(speed/maxspeed)
+if speed > maxspeed then
+speed = maxspeed
+end
+elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then
+speed = speed-1
+if speed < 0 then
+speed = 0
+end
+end
+if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then
+bv.velocity = ((game:GetService("Workspace").CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game:GetService("Workspace").CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game:GetService("Workspace").CurrentCamera.CoordinateFrame.p))*speed
+lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r}
+elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then
+bv.velocity = ((game:GetService("Workspace").CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game:GetService("Workspace").CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game:GetService("Workspace").CurrentCamera.CoordinateFrame.p))*speed
+else
+bv.velocity = Vector3.new(0,0.1,0)
+end
+bg.cframe = game:GetService("Workspace").CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0)
+until not flying
+ctrl = {f = 0, b = 0, l = 0, r = 0}
+lastctrl = {f = 0, b = 0, l = 0, r = 0}
+speed = 0
+bg:Destroy()
+bv:Destroy()
+--plr.Character.Humanoida.PlatformStand = false
+end
+mouse.KeyDown:connect(function(key)
+if key:lower() == "b" then
+if flying then flying = false
+else
+flying = true
+Fly()
+end
+elseif key:lower() == "w" then
+ctrl.f = 1
+elseif key:lower() == "s" then
+ctrl.b = -1
+elseif key:lower() == "a" then
+ctrl.l = -1
+elseif key:lower() == "d" then
+ctrl.r = 1
+end
+end)
+mouse.KeyUp:connect(function(key)
+if key:lower() == "w" then
+ctrl.f = 0
+elseif key:lower() == "s" then
+ctrl.b = 0
+elseif key:lower() == "a" then
+ctrl.l = 0
+elseif key:lower() == "d" then
+ctrl.r = 0
+end
+end)
+Fly()
+end)
+
+local Section = Tab:NewSection("Settings FPS")
+
+Section:NewButton("Show Fps", " ", function()
+repeat wait() until game:IsLoaded() wait(2)
+local ScreenGui = Instance.new("ScreenGui")
+local Fps = Instance.new("TextLabel")
+
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Fps.Name = "Fps"
+Fps.Parent = ScreenGui
+Fps.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+Fps.BackgroundTransparency = 1.000
+Fps.Position = UDim2.new(0.786138654, 0, 0, 0)
+Fps.Size = UDim2.new(0, 125, 0, 25)
+Fps.Font = Enum.Font.SourceSans
+Fps.TextColor3 = Color3.fromRGB(0, 255, 0)
+Fps.TextScaled = true
+Fps.TextSize = 14.000
+Fps.TextWrapped = true
+
+local script = Instance.new('LocalScript', Fps)
+local RunService = game:GetService("RunService")
+RunService.RenderStepped:Connect(function(frame) -- This will fire every time a frame is rendered
+script.Parent.Text = ("FPS: "..math.round(1/frame)) 
+end)
+
+end)
+Section:NewSlider("UnLock FPS", "FPS Unlock", 300, 15, function(FPSS)
+    setfpscap(FPSS)
+end)
+
+Section:NewButton("FPS BOOSTER", " ", function()
+    _G.Settings = {
+        Players = {
+            ["Ignore Me"] = true, -- Ignore your Character
+            ["Ignore Others"] = true -- Ignore other Characters
+        },
+        Meshes = {
+            Destroy = true, -- Destroy Meshes
+            LowDetail = true -- Low detail meshes (NOT SURE IT DOES ANYTHING)
+        },
+        Images = {
+            Invisible = true, -- Invisible Images
+            LowDetail = false, -- Low detail images (NOT SURE IT DOES ANYTHING)
+            Destroy = true, -- Destroy Images
+        },
+        Other = {
+            ["No Particles"] = true, -- Disables all ParticleEmitter, Trail, Smoke, Fire and Sparkles
+            ["No Camera Effects"] = true, -- Disables all PostEffect's (Camera/Lighting Effects)
+            ["No Explosions"] = true, -- Makes Explosion's invisible
+            ["No Clothes"] = true, -- Removes Clothing from the game
+            ["Low Water Graphics"] = true, -- Removes Water Quality
+            ["No Shadows"] = true, -- Remove Shadows
+            ["Low Rendering"] = true, -- Lower Rendering
+            ["Low Quality Parts"] = true -- Lower quality parts
+        }
+    }
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Main/main/FPSB.lua"))()
+end)
+
+local Tab = Window:NewTab("Farm")
+
+local Section = Tab:NewSection("Auto Farm")
+
+Section:NewToggle("Grass", " ",function(Grassa)
+    _G.GrassF = (Grassa)
+end)
+
+Section:NewToggle("Coin", " ",function(CoinA)
+    _G.FarmSS = (CoinA)
+end)
+
+spawn(function()
+	while wait() do
+		if _G.GrassF then
+      local tpcows = game:GetService("Workspace").Grass --copypath มา
+      local part = tpcows:FindFirstChild("_grass") --ชื่อไอสัส
+
+      if part and part:IsA("BasePart") then -- ไม่ต้องเปลี่ยน
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame
+      end
+      wait(0.1)
+		else
+		end
+	end
+end)
+
+spawn(function()
+	while wait() do
+		if _G.FarmSS then
+      local tpcows = game:GetService("Workspace") --copypath มา
+      local part = tpcows:FindFirstChild("Coin") --ชื่อไอสัส
+
+      if part and part:IsA("BasePart") then -- ไม่ต้องเปลี่ยน
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame
+      end
+      wait(0.1)
+		else
+		end
+	end
+end)
+
+local Tab = Window:NewTab("Quest")
+
+local Section = Tab:NewSection("Quest")
+
+Section:NewToggle("Block Race Car", " ",function(BlockC)
+    _G.Farm = (BlockC)
+end)
+
+spawn(function()
+	while wait() do
+		if _G.Farm then
+      local tpcows = game:GetService("Workspace").BoxFolder --copypath มา
+      local part = tpcows:FindFirstChild("Part") --ชื่อไอสัส
+
+      if part and part:IsA("BasePart") then -- ไม่ต้องเปลี่ยน
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame
+      end
+      wait(0.1)
+		else
+		end
+	end
+end)
+
+
+
+local Tab = Window:NewTab("Virtual User")
+
+local Section = Tab:NewSection("No Cooldown")
+
+Section:NewButton("Auto Interact [E] (Beta)", false,function()
+    while wait() do
+		while wait() do
+            local virtualUser = game:GetService('VirtualUser')
+            virtualUser:CaptureController()
+            virtualUser:SetKeyDown('0x65')
+            wait(0.01)
+            virtualUser:SetKeyUp('0x65')
+        end
+    end
+end)
+
+Section:NewButton("Remove Cooldown", " ", function()
+    game:GetService("ProximityPromptService").PromptButtonHoldBegan:Connect(function(prompt)
+        fireproximityprompt(prompt)
+    end)
+end)
+
+local Section = Tab:NewSection("Secerity Kick")
+
+Section:NewToggle("Secerity (1)", "If Player More 1 Player = Kick", function(secerity)
+    _G.SecerityKick = (secerity)
+end)
+
+spawn(function()
+    pcall(function()
+        while true do
+            local playermore = #game.Players:GetPlayers() > 1
+            if _G.SecerityKick then
+                local args = {
+                        [1] = "*"
+                    }
+                    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.MutePlayerRequest:InvokeServer(unpack(args))
+                if playermore then
+                    game.Players.LocalPlayer:Kick("There is more than 1 player on the server. automatic kick works")
+                end
+            end
+            wait(1)
+        end
+    end)
+end)
+
+local Tab = Window:NewTab("Music")
+local Section = Tab:NewSection("Music")
+
+Section:NewTextBox("Music ID", "press enter", function(MusicID)
+    MusicS.SoundId = "rbxassetid://"..MusicID
+end)
+
+Section:NewToggle("Start", " ", function(MusicT)
+    MusicS.Playing = (MusicT)
+end)
+
+Section:NewToggle("Looped", "", function(Loops)
+    MusicS.Looped = (Loops)
+end)
+
+Section:NewSlider("Volume", "", 100, 0, function(Volumes)
+    MusicS.Volume = (Volumes)
+end)
+
+local Tab = Window:NewTab("Player")
+
+Section:NewSlider("FOV", " ", 120, 70, function(Fovs)
+    workspace.Camera.FieldOfView = (Fovs)
+end)
+
+local Section = Tab:NewSection("🎛 All Deffault")
+
+Section:NewButton("Deffault", "WalksSpeed & JumpPower Deffault", function() 
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50 end)
+
+local Section = Tab:NewSection("⚡ WalksSpeed")
+
+Section:NewSlider("Speed", "WalksSpeed", 300, 16, function(WalksSpeeds)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (WalksSpeeds)
+end)
+
+local Section = Tab:NewSection("🦵 JumpPower")
+
+Section:NewSlider("JumpPower", "JumpPower", 500, 50, function(JumpPowers)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = (JumpPowers)
+end)
+
+local Section = Tab:NewSection("Spectate Player")
+Section:NewButton("Spectate", " ", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure/main/Spectate%20Player"))() end)
+
+local Section = Tab:NewSection("Teleport Player !")
+
+
+Plr = {}
+for i,v in pairs(game:GetService("Players"):GetChildren()) do
+    table.insert(Plr,v.Name) 
+end
+
+local drop = Section:NewDropdown("Select Player!", "Click To Select", (Plr), function(t)
+   PlayerTP = (t)
+end)
+Section:NewButton("Teleport", "", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[PlayerTP].Character.HumanoidRootPart.CFrame
+end)
+Section:NewToggle("Auto Teleport", "", function(t)
+_G.TPPlayer = (t)
+while _G.TPPlayer do wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[PlayerTP].Character.HumanoidRootPart.CFrame
+end
+end)
+
+local Tab = Window:NewTab("Main") 
+
+local Section = Tab:NewSection("Chat SPY")
+Section:NewButton("Click", "chat /spy to toggle!", function() 
+
+enabled = true --chat "/spy" to toggle!
+spyOnMyself = true --if true will check your messages too
+public = false --if true will chat the logs publicly (fun, risky)
+publicItalics = true --if true will use /me to stand out
+privateProperties = { --customize private logs
+	Color = Color3.fromRGB(0,255,255); 
+	Font = Enum.Font.SourceSansBold;
+	TextSize = 18;
+}
+
+
+local StarterGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer or Players:GetPropertyChangedSignal("LocalPlayer"):Wait() or Players.LocalPlayer
+local saymsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
+local getmsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("OnMessageDoneFiltering")
+local instance = (_G.chatSpyInstance or 0) + 1
+_G.chatSpyInstance = instance
+
+local function onChatted(p,msg)
+	if _G.chatSpyInstance == instance then
+		if p==player and msg:lower():sub(1,4)=="/spy" then
+			enabled = not enabled
+			wait(0.3)
+			privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
+			StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+		elseif enabled and (spyOnMyself==true or p~=player) then
+			msg = msg:gsub("[\n\r]",''):gsub("\t",' '):gsub("[ ]+",' ')
+			local hidden = true
+			local conn = getmsg.OnClientEvent:Connect(function(packet,channel)
+				if packet.SpeakerUserId==p.UserId and packet.Message==msg:sub(#msg-#packet.Message+1) and (channel=="All" or (channel=="Team" and public==false and Players[packet.FromSpeaker].Team==player.Team)) then
+					hidden = false
+				end
+			end)
+			wait(1)
+			conn:Disconnect()
+			if hidden and enabled then
+				if public then
+					saymsg:FireServer((publicItalics and "/me " or '').."{SPY} [".. p.Name .."]: "..msg,"All")
+				else
+					privateProperties.Text = "{SPY} [".. p.Name .."]: "..msg
+					StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+				end
+			end
+		end
+	end
+end
+
+for _,p in ipairs(Players:GetPlayers()) do
+	p.Chatted:Connect(function(msg) onChatted(p,msg) end)
+end
+Players.PlayerAdded:Connect(function(p)
+	p.Chatted:Connect(function(msg) onChatted(p,msg) end)
+end)
+privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
+StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+if not player.PlayerGui:FindFirstChild("Chat") then wait(3) end
+local chatFrame = player.PlayerGui.Chat.Frame
+chatFrame.ChatChannelParentFrame.Visible = true
+chatFrame.ChatBarParentFrame.Position = chatFrame.ChatChannelParentFrame.Position+UDim2.new(UDim.new(),chatFrame.ChatChannelParentFrame.Size.Y)
+end)
+
+local Section = Tab:NewSection("Server Hop")
+
+Section:NewButton("Click", " ", function() 
+    Time = 1 
+    repeat wait() until game:IsLoaded()
+    wait(Time)
+    local PlaceID = game.PlaceId
+    local AllIDs = {}
+    local foundAnything = ""
+    local actualHour = os.date("!*t").hour
+    local Deleted = false
+    function TPReturner()
+       local Site;
+       if foundAnything == "" then
+           Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+       else
+           Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+       end
+       local ID = ""
+       if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+           foundAnything = Site.nextPageCursor
+       end
+       local num = 0;
+       for i,v in pairs(Site.data) do
+           local Possible = true
+           ID = tostring(v.id)
+           if tonumber(v.maxPlayers) > tonumber(v.playing) then
+               for _,Existing in pairs(AllIDs) do
+                   if num ~= 0 then
+                       if ID == tostring(Existing) then
+                           Possible = false
+                       end
+                   else
+                       if tonumber(actualHour) ~= tonumber(Existing) then
+                           local delFile = pcall(function()
+                               delfile("NotSameServers.json")
+                               AllIDs = {}
+                               table.insert(AllIDs, actualHour)
+                           end)
+                       end
+                   end
+                   num = num + 1
+               end
+               if Possible == true then
+                   table.insert(AllIDs, ID)
+                   wait()
+                   pcall(function()
+                       writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                       wait()
+                       game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                   end)
+                   wait(2)
+               end
+           end
+       end
+    end
+    
+    function Teleport()
+       while wait() do
+           pcall(function()
+               TPReturner()
+               if foundAnything ~= "" then
+                   TPReturner()
+               end
+           end)
+       end
+    end
+    
+    -- If you'd like to use a script before server hopping (Like a Automatic Chest collector you can put the Teleport() after it collected everything.
+    Teleport()
+end)
+
+
+local Section = Tab:NewSection("Rejoin")
+
+Section:NewButton("Click", "ออกเข้าใหม่", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/blox-fruit-fake-level-/main/Rejoin%20Game.lua"))() end)
+
+local Section = Tab:NewSection("X-Ray")
+Section:NewButton("Click", "Press X To X-Ray", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure/main/X-Ray.lua"))() end)
+		
+local Section = Tab:NewSection("Infinite Yield")
+
+Section:NewButton("Click", "เปิด Infinite Yield", function() loadstring(game:HttpGet(("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"),true ))() end)
+
+local Section = Tab:NewSection("Anit AFK")
+  
+Section:NewButton("Click", "เปิด Anit AFK", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Roblox-Script-AFK/main/Script%20AFK.lua"))() end)
+
+local Section = Tab:NewSection("Remove Fog")
+  
+Section:NewButton("Click", "", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure/main/Remove%20Fog.lua"))() end)
+
+local Tab = Window:NewTab("Script Hub")
+
+local Section = Tab:NewSection("Infinite Yield")
+Section:NewButton("Click", "เปิด Infinite Yield", function() loadstring(game:HttpGet(("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"),true ))() end)
+
+local Section = Tab:NewSection("Emotes")
+Section:NewButton("Click", "press to </ม to Open", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure/main/Emotes.lua"))() end)
+
+local Section = Tab:NewSection("Orca")
+Section:NewButton("Click", " ", function() loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/peatchXD/Main/main/Orca"))()end)
+
+local Tab = Window:NewTab("Credit") 
+local Section = Tab:NewSection("By: peatchXD#6075")
+Section:NewKeybind("press to change button", "KeybindInfo", Enum.KeyCode.F, function() Library:ToggleUI() end)  

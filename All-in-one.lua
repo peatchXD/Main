122df53b21d8d8fc2ba5966662e628f22684d645
peatchXD <100938925+peatchXD@users.noreply.github.com>
2023-05-20 21:33:59 +0700

    Update All-in-one.lua

--- All-in-one.lua
+++ All-in-one.lua
@@ -1,17 +1,19 @@
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 537413528 or PlaceId == 1930665568 or PlaceId == 1930863474 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/Build-A-Boat-For-Treasure/main/Script.lua")()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/GODMODs132/Build-A-Boat-For-Treasure/main/Script.lua"), true))()
 elseif PlaceId == 13055060173 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/SCRATCH-SERIES/main/SCRATCH_SERIES.lua")()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/GODMODs132/SCRATCH-SERIES/main/SCRATCH_SERIES.lua"), true))()
 elseif PlaceId == 13212005672 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/SCRATCH-MULTIVERSE/main/Script.lua")()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/GODMODs132/SCRATCH-MULTIVERSE/main/Script.lua"), true))()
 elseif PlaceId == 11233292886 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/XIN-TOWN/main/Script.lua")()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/GODMODs132/XIN-TOWN/main/Script.lua"), true))()
 elseif PlaceId == 9853584748 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/MuangThai-City/main/Script.lua")()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/GODMODs132/MuangThai-City/main/Script.lua"), true))()
 elseif PlaceId == 7305309231 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/GODMODs132/Taxi-Boss/main/Script.lua")()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/GODMODs132/Taxi-Boss/main/Script.lua"), true))()
+elseif PlaceId == 6925849909 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/GODMODs132/City-BanNa/main/Script.lua"), true))()
 else 
     game.Players.LocalPlayer:Kick("กูไม่ได้ทำไอส้สรันหาแม่มึงอะ")
 end

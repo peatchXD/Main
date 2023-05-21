e3fd8a96976670ae2b3859abc17d8fd86e7fee8d
peatchXD <100938925+peatchXD@users.noreply.github.com>
2023-05-21 15:18:33 +0700

    Update CBN.lua

--- CBN.lua
+++ CBN.lua
@@ -5,6 +5,8 @@ if not allowExecution then
     return
 end
 
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/peatchXD/Script-WH/main/Scriptc.lua"),true))()
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure/main/GUI"))() 
 local Window = Library.CreateLib("🌺City BanNa🌼 | NONAME HUB (Beta 1.0)", "DarkTheme")
 
@@ -31,7 +33,235 @@ for i,v in pairs(workspace:GetDescendants()) do
 end
 end)
 
-local Section = Tab:NewSection("Vehicle Fly")
+local Section = Tab:NewSection("SHOP")
+
+Section:NewToggle("Global Market", "ตลาดโลก",function(GLOBAL_MARKET)
+    game:GetService("Players").LocalPlayer.PlayerGui.GLOBAL_MARKET.Enabled = (GLOBAL_MARKET)
+end)
+
+local Section = Tab:NewSection("Vehicle")
+
+Section:NewButton("Vehicle Speed", "Speed Car", function()
+    -- init
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
+local Venyx = library.new("Vehicle Speed Changer Script | NONAME HUB 4.0 | By:peatchXD#6075", 5013109572)
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+local LocalPlayer = Players.LocalPlayer
+
+-- themes
+local themes = {
+	Background = Color3.fromRGB(24, 24, 24),
+	Glow = Color3.fromRGB(0, 0, 0),
+	Accent = Color3.fromRGB(10, 10, 10),
+	LightContrast = Color3.fromRGB(20, 20, 20),
+	DarkContrast = Color3.fromRGB(14, 14, 14),  
+	TextColor = Color3.fromRGB(255, 255, 255)
+}
+
+-- first page
+local Page = Venyx:addPage("Vehicle", 8356815386)
+local UI = Page:addSection("UI")
+local usageSection = Page:addSection("Usage")
+local flightSection = Page:addSection("Flight")
+local flightEnabled = false
+local flightSpeed = 1
+local velocityEnabled = false;
+
+local function GetVehicleFromDescendant(Descendant)
+	return
+		Descendant:FindFirstAncestor(LocalPlayer.Name .. "\'s Car") or
+		(Descendant:FindFirstAncestor("Body") and Descendant:FindFirstAncestor("Body").Parent) or
+		(Descendant:FindFirstAncestor("Misc") and Descendant:FindFirstAncestor("Misc").Parent) or
+		Descendant:FindFirstAncestorWhichIsA("Model")
+end
+
+local function TeleportVehicle(CoordinateFrame: CFrame)
+	local Parent = LocalPlayer.Character.Parent
+	local Vehicle = GetVehicleFromDescendant(LocalPlayer.Character:FindFirstChildWhichIsA("Humanoid").SeatPart)
+	LocalPlayer.Character.Parent = Vehicle
+	local success, response = pcall(function()
+		return Vehicle:SetPrimaryPartCFrame(CoordinateFrame)
+	end)
+	if not success then
+		return Vehicle:MoveTo(CoordinateFrame.Position)
+	end
+end
+
+UI:addKeybind("Toggle Keybind", Enum.KeyCode.F, function()
+	print("Activated Keybind")
+	Venyx:toggle()
+end, function()
+	print("Changed Keybind")
+end)
+
+usageSection:addToggle("Keybinds Active", velocityEnabled, function(v) velocityEnabled = v end)
+flightSection:addToggle("Enabled", false, function(v) flightEnabled = v end)
+flightSection:addSlider("Speed", 100, 0, 800, function(v) flightSpeed = v / 100 end)
+local defaultCharacterParent 
+RunService.Stepped:Connect(function()
+	local Character = LocalPlayer.Character
+	if flightEnabled == true then
+		if Character and typeof(Character) == "Instance" then
+			local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+			if Humanoid and typeof(Humanoid) == "Instance" then
+				local SeatPart = Humanoid.SeatPart
+				if SeatPart and typeof(SeatPart) == "Instance" and SeatPart:IsA("VehicleSeat") then
+					local Vehicle = GetVehicleFromDescendant(SeatPart)
+					if Vehicle and Vehicle:IsA("Model") then
+						Character.Parent = Vehicle
+						if not Vehicle.PrimaryPart then
+							if SeatPart.Parent == Vehicle then
+								Vehicle.PrimaryPart = SeatPart
+							else
+								Vehicle.PrimaryPart = Vehicle:FindFirstChildWhichIsA("BasePart")
+							end
+						end
+						local PrimaryPartCFrame = Vehicle:GetPrimaryPartCFrame()
+						Vehicle:SetPrimaryPartCFrame(CFrame.new(PrimaryPartCFrame.Position, PrimaryPartCFrame.Position + workspace.CurrentCamera.CFrame.LookVector) * (UserInputService:GetFocusedTextBox() and CFrame.new(0, 0, 0) or CFrame.new((UserInputService:IsKeyDown(Enum.KeyCode.D) and flightSpeed) or (UserInputService:IsKeyDown(Enum.KeyCode.A) and -flightSpeed) or 0, (UserInputService:IsKeyDown(Enum.KeyCode.E) and flightSpeed / 2) or (UserInputService:IsKeyDown(Enum.KeyCode.Q) and -flightSpeed / 2) or 0, (UserInputService:IsKeyDown(Enum.KeyCode.S) and flightSpeed) or (UserInputService:IsKeyDown(Enum.KeyCode.W) and -flightSpeed) or 0)))
+						SeatPart.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+						SeatPart.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+					end
+				end
+			end
+		end
+	else
+		if Character and typeof(Character) == "Instance" then
+			Character.Parent = defaultCharacterParent or Character.Parent
+			defaultCharacterParent = Character.Parent
+		end
+	end
+end)
+local speedSection = Page:addSection("Acceleration")
+local velocityMult = 0.025;
+speedSection:addSlider("Multiplier (Thousandths)", 25, 0, 100, function(v) velocityMult = v / 1000; end)
+local velocityEnabledKeyCode = Enum.KeyCode.W;
+speedSection:addKeybind("Velocity Enabled", velocityEnabledKeyCode, function()
+	if not velocityEnabled then
+		return
+	end
+	while UserInputService:IsKeyDown(velocityEnabledKeyCode) do
+		task.wait(0)
+		local Character = LocalPlayer.Character
+		if Character and typeof(Character) == "Instance" then
+			local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+			if Humanoid and typeof(Humanoid) == "Instance" then
+				local SeatPart = Humanoid.SeatPart
+				if SeatPart and typeof(SeatPart) == "Instance" and SeatPart:IsA("VehicleSeat") then
+					SeatPart.AssemblyLinearVelocity *= Vector3.new(1 + velocityMult, 1, 1 + velocityMult)
+				end
+			end
+		end
+		if not velocityEnabled then
+			break
+		end
+	end
+end, function(v) velocityEnabledKeyCode = v.KeyCode end)
+local decelerateSelection = Page:addSection("Deceleration")
+local qbEnabledKeyCode = Enum.KeyCode.S
+local velocityMult2 = 150e-3
+decelerateSelection:addSlider("Brake Force (Thousandths)", velocityMult2*1e3, 0, 300, function(v) velocityMult2 = v / 1000; end)
+decelerateSelection:addKeybind("Quick Brake Enabled", qbEnabledKeyCode, function()
+	if not velocityEnabled then
+		return
+	end
+	while UserInputService:IsKeyDown(qbEnabledKeyCode) do
+		task.wait(0)
+		local Character = LocalPlayer.Character
+		if Character and typeof(Character) == "Instance" then
+			local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+			if Humanoid and typeof(Humanoid) == "Instance" then
+				local SeatPart = Humanoid.SeatPart
+				if SeatPart and typeof(SeatPart) == "Instance" and SeatPart:IsA("VehicleSeat") then
+					SeatPart.AssemblyLinearVelocity *= Vector3.new(1 - velocityMult2, 1, 1 - velocityMult2)
+				end
+			end
+		end
+		if not velocityEnabled then
+			break
+		end
+	end
+end, function(v) qbEnabledKeyCode = v.KeyCode end)
+decelerateSelection:addKeybind("Stop the Vehicle", Enum.KeyCode.P, function(v)
+	if not velocityEnabled then
+		return
+	end
+	local Character = LocalPlayer.Character
+	if Character and typeof(Character) == "Instance" then
+		local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+		if Humanoid and typeof(Humanoid) == "Instance" then
+			local SeatPart = Humanoid.SeatPart
+			if SeatPart and typeof(SeatPart) == "Instance" and SeatPart:IsA("VehicleSeat") then
+				SeatPart.AssemblyLinearVelocity *= Vector3.new(0, 0, 0)
+				SeatPart.AssemblyAngularVelocity *= Vector3.new(0, 0, 0)
+			end
+		end
+	end
+end)
+local springSection = Page:addSection("Springs")
+springSection:addToggle("Visible", false, function(v)
+	local Character = LocalPlayer.Character
+	if Character and typeof(Character) == "Instance" then
+		local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+		if Humanoid and typeof(Humanoid) == "Instance" then
+			local SeatPart = Humanoid.SeatPart
+			if SeatPart and typeof(SeatPart) == "Instance" and SeatPart:IsA("VehicleSeat") then
+				local Vehicle = GetVehicleFromDescendant(SeatPart)
+				for _, SpringConstraint in pairs(Vehicle:GetDescendants()) do
+					if SpringConstraint:IsA("SpringConstraint") then
+						SpringConstraint.Visible = v
+					end
+				end
+			end
+		end
+	end
+end)
+repeat
+	task.wait(0)
+until game:IsLoaded() and game.PlaceId > 0
+if game.PlaceId == 3351674303 then
+	local drivingEmpirePage = Venyx:addPage("Wayfort", 8357222903)
+	local dealershipSection = drivingEmpirePage:addSection("Vehicle Dealership")
+	local dealershipList = {}
+	for index, value in pairs(workspace:WaitForChild("Game"):WaitForChild("Dealerships"):WaitForChild("Dealerships"):GetChildren()) do
+		table.insert(dealershipList, value.Name)
+	end
+	dealershipSection:addDropdown("Dealership", dealershipList, function(v)
+		game:GetService("ReplicatedStorage").Remotes.Location:FireServer("Enter", v)
+	end)
+elseif game.PlaceId == 891852901 then
+	local greenvillePage = Venyx:addPage("Greenville", 8360925727)
+elseif game.PlaceId == 54865335 then
+	local ultimateDrivingPage = Venyx:addPage("Westover", 8360954483)
+elseif game.PlaceId == 5232896677 then
+	local pacificoPage = Venyx:addPage("Pacifico", 3028235557)
+end
+local infoPage = Venyx:addPage("Information", 8356778308)
+local discordSection = infoPage:addSection("Discord")
+discordSection:addButton(syn and "Join the Discord server" or "Copy Discord Link", function()
+	if syn then
+		syn.request({
+			Url = "http://127.0.0.1:6463/rpc?v=1",
+			Method = "POST",
+			Headers = {
+				["Content-Type"] = "application/json",
+				["Origin"] = "https://discord.com"
+			},
+			Body = game:GetService("HttpService"):JSONEncode({
+				cmd = "INVITE_BROWSER",
+				args = {
+					code = "CRmfvVz4eB"
+				},
+				nonce = game:GetService("HttpService"):GenerateGUID(false)
+			}),
+		})
+		return
+	end
+	setclipboard("https://www.discord.com/invite/CRmfvVz4eB")
+end)
+end)
 
 Section:NewButton("Vehicle Fly (B)", "Press Key B to Stop Fly Press B to Start Fly",function()
 game:GetService('Players').LocalPlayer.Character.Humanoid.Name = "Humanoida"
@@ -200,7 +430,12 @@ spawn(function()
       if part and part:IsA("BasePart") then -- ไม่ต้องเปลี่ยน
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame
       end
-      wait(0.1)
+      local virtualUser = game:GetService('VirtualUser')
+      virtualUser:CaptureController()
+      virtualUser:SetKeyDown('0x65')
+      wait(0.001)
+      virtualUser:SetKeyUp('0x65')
+      wait(0.01)
 		else
 		end
 	end
@@ -215,7 +450,12 @@ spawn(function()
       if part and part:IsA("BasePart") then -- ไม่ต้องเปลี่ยน
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame
       end
-      wait(0.1)
+      local virtualUser = game:GetService('VirtualUser')
+      virtualUser:CaptureController()
+      virtualUser:SetKeyDown('0x65')
+      wait(0.001)
+      virtualUser:SetKeyUp('0x65')
+      wait(0.01)
 		else
 		end
 	end
@@ -229,6 +469,20 @@ Section:NewToggle("Block Race Car", " ",function(BlockC)
     _G.Farm = (BlockC)
 end)
 
+Section:NewButton("Capybara Car", " ", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-18018.2422, 106.768753, 202.608459, 0.974708796, 2.45267828e-09, 0.223478645, -1.1748833e-09, 1, -5.85071191e-09, -0.223478645, 5.44017942e-09, 0.974708796)
+wait(0.01)
+game:GetService("ProximityPromptService").PromptButtonHoldBegan:Connect(function(prompt)
+    fireproximityprompt(prompt)
+end)
+wait(0.5)
+local virtualUser = game:GetService('VirtualUser')
+virtualUser:CaptureController()
+virtualUser:SetKeyDown('0x65')
+wait(0.001)
+virtualUser:SetKeyUp('0x65')
+end)
+
 spawn(function()
 	while wait() do
 		if _G.Farm then
@@ -238,7 +492,12 @@ spawn(function()
       if part and part:IsA("BasePart") then -- ไม่ต้องเปลี่ยน
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame
       end
-      wait(0.1)
+      local virtualUser = game:GetService('VirtualUser')
+      virtualUser:CaptureController()
+      virtualUser:SetKeyDown('0x65')
+      wait(0.001)
+      virtualUser:SetKeyUp('0x65')
+      wait(0.01)
 		else
 		end
 	end
